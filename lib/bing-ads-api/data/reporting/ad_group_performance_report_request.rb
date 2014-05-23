410035953840460d6c7275fc7cad0bdcd2be6412
Andreas Robecke <andreas.robecke@makandra.de>
2014-05-23 16:40:13 +0200

    wip

--- lib/bing-ads-api/data/reporting/ad_group_performance_report_request.rb
+++ lib/bing-ads-api/data/reporting/ad_group_performance_report_request.rb
@@ -4,40 +4,51 @@ module BingAdsApi
   class AdGroupPerformanceReportRequest < BingAdsApi::PerformanceReportRequest
     # Valid Columns for this report request
     COLUMNS = BingAdsApi::Config.instance.
-      reporting_constants['account_performance_report']['columns']
+      reporting_constants['ad_group_performance_report']['columns']
 
     # Valid Filters for this report request
     FILTERS = BingAdsApi::Config.instance.
-      reporting_constants['account_performance_report']['filter']
+      reporting_constants['ad_group_performance_report']['filter']
 
     def initialize(attributes={})
+      # raise Exception.new("Invalid columns") if !valid_columns(COLUMNS, attributes[:columns])
+      # raise Exception.new("Invalid filters") if !valid_filter(FILTERS, attributes[:filter])
+      # raise Exception.new("Invalid scope") if !valid_scope(attributes[:scope])
       super(attributes)
     end
 
     def to_hash(keys = :underscore)
       hash = super(keys)
-
-      binding.pry
       hash[get_attribute_key('columns', keys)] =
         columns_to_hash(COLUMNS, columns, keys)
       hash[get_attribute_key('filter', keys)] =
         filter_to_hash(FILTERS, keys)
       hash[get_attribute_key('scope', keys)] = scope_to_hash(keys)
       hash["@xsi:type"] = type_attribute_for_soap
+      puts "Andys gem:"
+      binding.pry
       return hash
     end
 
+
     private
 
+    # def valid_scope(scope)
+    #   raise Exception.new("Invalid scope: no account_ids key") if !scope.key?(:account_ids)
+    #   raise Exception.new("Invalid scope: no ad_groups key") if !scope.key?(:campaigns)
+    #   return true
+    # end
+
     def scope_to_hash(keys_case=:underscore)
-      return { get_attribute_key('account_ids', keys_case) =>
-                 {"ins0:long" => object_to_hash(scope[:account_ids], keys_case)} }
+      return {
+        get_attribute_key('account_ids', keys_case) => {"ins0:long" => object_to_hash(scope[:account_ids], keys_case)}
+      }
     end
 
     def type_attribute_for_soap
       return BingAdsApi::ClientProxy::NAMESPACE.to_s + ":" +
         BingAdsApi::Config.instance.
-          reporting_constants['account_performance_report']['type']
+          reporting_constants['ad_group_performance_report']['type']
     end
 
   end
