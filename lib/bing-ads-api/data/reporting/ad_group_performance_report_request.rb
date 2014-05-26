7521c1269c939d924932746924d67b406a3f6a43
Andreas Robecke <andreas.robecke@makandra.de>
2014-05-26 15:12:41 +0200

    wip

--- lib/bing-ads-api/data/reporting/ad_group_performance_report_request.rb
+++ lib/bing-ads-api/data/reporting/ad_group_performance_report_request.rb
@@ -11,9 +11,9 @@ module BingAdsApi
       reporting_constants['ad_group_performance_report']['filter']
 
     def initialize(attributes={})
-      # raise Exception.new("Invalid columns") if !valid_columns(COLUMNS, attributes[:columns])
-      # raise Exception.new("Invalid filters") if !valid_filter(FILTERS, attributes[:filter])
-      # raise Exception.new("Invalid scope") if !valid_scope(attributes[:scope])
+      raise Exception.new("Invalid columns") if !valid_columns(COLUMNS, attributes[:columns])
+      raise Exception.new("Invalid filters") if !valid_filter(FILTERS, attributes[:filter])
+      raise Exception.new("Invalid scope") if !valid_scope(attributes[:scope])
       super(attributes)
     end
 
@@ -25,26 +25,26 @@ module BingAdsApi
         filter_to_hash(FILTERS, keys)
       hash[get_attribute_key('scope', keys)] = scope_to_hash(keys)
       hash["@xsi:type"] = type_attribute_for_soap
-      return hash
+
+      hash
     end
 
 
     private
 
-    # def valid_scope(scope)
-    #   raise Exception.new("Invalid scope: no account_ids key") if !scope.key?(:account_ids)
-    #   raise Exception.new("Invalid scope: no ad_groups key") if !scope.key?(:campaigns)
-    #   return true
-    # end
+    def valid_scope(scope)
+      raise Exception.new("Invalid scope: no account_ids key") if !scope.key?(:account_ids)
+      true
+    end
 
     def scope_to_hash(keys_case=:underscore)
-      return {
+      {
         get_attribute_key('account_ids', keys_case) => {"ins0:long" => object_to_hash(scope[:account_ids], keys_case)}
       }
     end
 
     def type_attribute_for_soap
-      return BingAdsApi::ClientProxy::NAMESPACE.to_s + ":" +
+      BingAdsApi::ClientProxy::NAMESPACE.to_s + ":" +
         BingAdsApi::Config.instance.
           reporting_constants['ad_group_performance_report']['type']
     end
