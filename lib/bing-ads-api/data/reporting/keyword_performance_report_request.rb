f31afc3d7ec4f6291506716d8085349e2cd74792
Amir Ghazvinian <amir.ghazvinian@gmail.com>
2015-11-24 10:49:52 -0800

    fixup keyword performance report

--- lib/bing-ads-api/data/reporting/keyword_performance_report_request.rb
+++ lib/bing-ads-api/data/reporting/keyword_performance_report_request.rb
@@ -11,9 +11,9 @@ module BingAdsApi
       reporting_constants['keyword_performance_report']['filter']
 
     def initialize(attributes={})
-      raise Exception.new("Invalid columns") if !valid_columns(COLUMNS, attributes[:columns])
-      raise Exception.new("Invalid filters") if !valid_filter(FILTERS, attributes[:filter])
-      raise Exception.new("Invalid scope") if !valid_scope(attributes[:scope])
+      #raise Exception.new("Invalid columns") if !valid_columns(COLUMNS, attributes[:columns])
+      #raise Exception.new("Invalid filters") if !valid_filter(FILTERS, attributes[:filter])
+      #raise Exception.new("Invalid scope") if !valid_scope(attributes[:scope])
       super(attributes)
     end
 
@@ -21,26 +21,31 @@ module BingAdsApi
       hash = super(keys)
       hash[get_attribute_key('columns', keys)] =
         columns_to_hash(COLUMNS, columns, keys)
-      hash[get_attribute_key('filter', keys)] =
-        filter_to_hash(FILTERS, keys)
-      hash[get_attribute_key('scope', keys)] = scope_to_hash(keys)
-      hash["@xsi:type"] = type_attribute_for_soap
-
+      filter_hash = filter_to_hash(FILTERS, filter, keys)
+      hash[get_attribute_key('filter', keys)] = filter_hash if filter_hash.present?
+      scope_hash = scope_to_hash(keys)
+      hash[get_attribute_key('scope', keys)] = scope_hash
+      hash['@xsi:type'] = type_attribute_for_soap
       hash
     end
-
 
     private
 
     def valid_scope(scope)
-      raise Exception.new("Invalid scope: no account_ids key") if !scope.key?(:account_ids)
+      #raise Exception.new("Invalid scope: no account_ids key") if !scope.key?(:account_ids)
       true
     end
 
     def scope_to_hash(keys_case=:underscore)
-      {
-        get_attribute_key('account_ids', keys_case) => {"ins0:long" => object_to_hash(scope[:account_ids], keys_case)}
-      }
+      return {} unless scope.present?
+      scope_hash = {}
+      account_ids_scope = object_to_hash(scope[:account_ids], keys_case)
+      scope_hash[get_attribute_key('account_ids', keys_case)] = {'ins0:long' => account_ids_scope} if account_ids_scope.present?
+      campaigns_scope = object_to_hash(scope[:campaigns], keys_case)
+      scope_hash[get_attribute_key('campaigns', keys_case)] = {'CampaignReportScope' =>  campaigns_scope} if campaigns_scope.present?
+      ad_group_scope = object_to_hash(scope[:ad_groups], keys_case)
+      scope_hash[get_attribute_key('ad_groups', keys_case)] = {'AdGroupReportScope' => ad_group_scope}
+      scope_hash
     end
 
     def type_attribute_for_soap
